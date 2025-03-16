9407c3f216971536bd6dcfb4f6e77c70dda3807d
Eric Salem <ericsalem@gmail.com>
2025-03-15 20:13:30 -0500

    newlib: riscv: Optimize strlen()
    The RISC-V Zbb extension provides instructions optimized for bit
    operations. Use them when available for the RISC-V port.

--- newlib/libc/machine/riscv/strlen.c
+++ newlib/libc/machine/riscv/strlen.c
@@ -30,24 +30,33 @@ size_t strlen(const char *str)
     } while ((uintptr_t)str & (sizeof (uintptr_t) - 1));
 
   uintptr_t *ps = (uintptr_t *)str;
-  while (!__libc_detect_null (*ps++))
+  uintptr_t psval;
+
+  while (!__libc_detect_null ((psval = *ps++)))
     ;
   asm volatile ("" : "+r"(ps)); /* prevent "optimization" */
 
   str = (const char *)ps;
   size_t ret = str - start, sp = sizeof (*ps);
 
-  char c0 = str[0 - sp], c1 = str[1 - sp], c2 = str[2 - sp], c3 = str[3 - sp];
-  if (c0 == 0)  return ret + 0 - sp;
-  if (c1 == 0)  return ret + 1 - sp;
-  if (c2 == 0)  return ret + 2 - sp;
-  if (c3 == 0)  return ret + 3 - sp;
+  #if __riscv_zbb
+    psval = ~__LIBC_RISCV_ZBB_ORC_B(psval);
+    psval = __LIBC_RISCV_ZBB_CNT_Z(psval);
 
-  c0 = str[4 - sp], c1 = str[5 - sp], c2 = str[6 - sp];
-  if (c0 == 0)  return ret + 4 - sp;
-  if (c1 == 0)  return ret + 5 - sp;
-  if (c2 == 0)  return ret + 6 - sp;
+    return ret + (psval >> 3) - sp;
+  #else
+    char c0 = str[0 - sp], c1 = str[1 - sp], c2 = str[2 - sp], c3 = str[3 - sp];
+    if (c0 == 0)  return ret + 0 - sp;
+    if (c1 == 0)  return ret + 1 - sp;
+    if (c2 == 0)  return ret + 2 - sp;
+    if (c3 == 0)  return ret + 3 - sp;
 
-  return ret + 7 - sp;
+    c0 = str[4 - sp], c1 = str[5 - sp], c2 = str[6 - sp];
+    if (c0 == 0)  return ret + 4 - sp;
+    if (c1 == 0)  return ret + 5 - sp;
+    if (c2 == 0)  return ret + 6 - sp;
+
+    return ret + 7 - sp;
+  #endif
 #endif /* not PREFER_SIZE_OVER_SPEED */
 }
--- newlib/libc/machine/riscv/sys/string.h
+++ newlib/libc/machine/riscv/sys/string.h
@@ -12,12 +12,44 @@
 #ifndef _SYS_STRING_H
 #define _SYS_STRING_H
 
+#if __riscv_zbb
+  #include <riscv_bitmanip.h>
+
+  // Determine which intrinsics to use based on word size and endianness
+  #if __riscv_xlen == 64
+    #define __LIBC_RISCV_ZBB_ORC_B       __riscv_orc_b_64
+
+    #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+      #define __LIBC_RISCV_ZBB_CNT_Z     __riscv_ctz_64
+    #else
+      #define __LIBC_RISCV_ZBB_CNT_Z     __riscv_clz_64
+    #endif
+  #else
+    #define __LIBC_RISCV_ZBB_ORC_B       __riscv_orc_b_32
+
+    #if __BYTE_ORDER__ == __ORDER_LITTLE_ENDIAN__
+      #define __LIBC_RISCV_ZBB_CNT_Z     __riscv_ctz_32
+    #else
+      #define __LIBC_RISCV_ZBB_CNT_Z     __riscv_clz_32
+    #endif
+  #endif
+#endif
+
 static __inline unsigned long __libc_detect_null(unsigned long w)
 {
+#if __riscv_zbb
+  /*
+    If there are any zeros in each byte of the register, all bits will
+    be unset for that byte value, otherwise, all bits will be set.
+    If the value is -1, all bits are set, meaning no null byte was found.
+  */
+  return __LIBC_RISCV_ZBB_ORC_B(w) != -1;
+#else
   unsigned long mask = 0x7f7f7f7f;
   if (sizeof (long) == 8)
     mask = ((mask << 16) << 16) | mask;
   return ~(((w & mask) + mask) | w | mask);
+#endif
 }
 
 #endif
