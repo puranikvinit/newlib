f7d41988e43461734f40253018c964d0e3a71fbe
m fally <marlene.fally@gmail.com>
2025-04-25 19:09:26 +0200

    RISC-V: memmove() speed optimized: Clean up code formatting
    Clean up the formatting of the code to make the implementation
    more readable.

--- newlib/libc/machine/riscv/memmove-stub.c
+++ newlib/libc/machine/riscv/memmove-stub.c
@@ -7,82 +7,68 @@
 
 /*SUPPRESS 20*/
 void *
-__inhibit_loop_to_libcall
-memmove (void *dst_void,
-	const void *src_void,
-	size_t length)
-{
-  char *dst = dst_void;
-  const char *src = src_void;
-  long *aligned_dst;
-  const long *aligned_src;
+__inhibit_loop_to_libcall memmove (void *dst_void, const void *src_void, size_t length) {
 
-  if (src < dst && dst < src + length)
-    {
-      /* Destructive overlap...have to copy backwards */
-      src += length;
-      dst += length;
+	char *dst = dst_void;
+	const char *src = src_void;
+	long *aligned_dst;
+	const long *aligned_src;
 
-      if (!TOO_SMALL_LITTLE_BLOCK(length) && !UNALIGNED_X_Y(src, dst))
-        {
-          aligned_dst = (long*)dst;
-          aligned_src = (long*)src;
+	if (src < dst && dst < src + length) {
+		/* Destructive overlap...have to copy backwards */
+		src += length;
+		dst += length;
 
-          /* Copy one long word at a time if possible.  */
-          while (!TOO_SMALL_LITTLE_BLOCK(length))
-            {
-              *--aligned_dst = *--aligned_src;
-              length -= LITTLE_BLOCK_SIZE;
-            }
+		if (!TOO_SMALL_LITTLE_BLOCK(length) && !UNALIGNED_X_Y(src, dst)) {
 
-          /* Pick up any residual with a byte copier.  */
-          dst = (char*)aligned_dst;
-          src = (char*)aligned_src;
-        }
+			aligned_dst = (long*)dst;
+			aligned_src = (long*)src;
 
-      while (length--)
-        {
-          *--dst = *--src;
-        }
-    }
-  else
-    {
-      /* Use optimizing algorithm for a non-destructive copy to closely 
-         match memcpy. If the size is small or either SRC or DST is unaligned,
-         then punt into the byte copy loop.  This should be rare.  */
-      if (!TOO_SMALL_LITTLE_BLOCK(length) && !UNALIGNED_X_Y(src, dst))
-        {
-          aligned_dst = (long*)dst;
-          aligned_src = (long*)src;
+			/* Copy one long word at a time if possible.  */
+			while (!TOO_SMALL_LITTLE_BLOCK(length)) {
+				*--aligned_dst = *--aligned_src;
+				length -= LITTLE_BLOCK_SIZE;
+			}
 
-          /* Copy 4X long words at a time if possible.  */
-          while (!TOO_SMALL_BIG_BLOCK(length))
-            {
-              *aligned_dst++ = *aligned_src++;
-              *aligned_dst++ = *aligned_src++;
-              *aligned_dst++ = *aligned_src++;
-              *aligned_dst++ = *aligned_src++;
-              length -= BIG_BLOCK_SIZE;
-            }
+			/* Pick up any residual with a byte copier.  */
+			dst = (char*)aligned_dst;
+			src = (char*)aligned_src;
+		}
 
-          /* Copy one long word at a time if possible.  */
-          while (!TOO_SMALL_LITTLE_BLOCK(length))
-            {
-              *aligned_dst++ = *aligned_src++;
-              length -= LITTLE_BLOCK_SIZE;
-            }
+		while (length--) { *--dst = *--src; }
+	}
+	else {
+		/* Use optimizing algorithm for a non-destructive copy to closely
+		   match memcpy. If the size is small or either SRC or DST is unaligned,
+		   then punt into the byte copy loop.  This should be rare.  */
+		if (!TOO_SMALL_LITTLE_BLOCK(length) && !UNALIGNED_X_Y(src, dst)) {
 
-          /* Pick up any residual with a byte copier.  */
-          dst = (char*)aligned_dst;
-          src = (char*)aligned_src;
-        }
+			aligned_dst = (long*)dst;
+			aligned_src = (long*)src;
 
-      while (length--)
-        {
-          *dst++ = *src++;
-        }
-    }
+			/* Copy 4X long words at a time if possible.  */
+			while (!TOO_SMALL_BIG_BLOCK(length)) {
+				*aligned_dst++ = *aligned_src++;
+				*aligned_dst++ = *aligned_src++;
+				*aligned_dst++ = *aligned_src++;
+				*aligned_dst++ = *aligned_src++;
+				length -= BIG_BLOCK_SIZE;
+			}
 
-  return dst_void;
+			/* Copy one long word at a time if possible.  */
+			while (!TOO_SMALL_LITTLE_BLOCK(length)) {
+				*aligned_dst++ = *aligned_src++;
+				length -= LITTLE_BLOCK_SIZE;
+			}
+
+			/* Pick up any residual with a byte copier.  */
+			dst = (char*)aligned_dst;
+			src = (char*)aligned_src;
+		}
+
+		while (length--) { *dst++ = *src++; }
+	}
+
+	return dst_void;
 }
 #endif /* not PREFER_SIZE_OVER_SPEED */
