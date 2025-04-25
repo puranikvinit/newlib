67b2389ed19650160a2af0e7e8061170424c8130
m fally <marlene.fally@gmail.com>
2025-04-25 19:25:42 +0200

    RISC-V: memmove() speed optimized: Change data types to fixed-length types
    Change all data types to fixed-length types and/or RISC-V-specific types
    to have more fine-grained control over the function.

--- newlib/libc/machine/riscv/memmove-stub.c
+++ newlib/libc/machine/riscv/memmove-stub.c
@@ -3,36 +3,47 @@
 #include <_ansi.h>
 #include <stddef.h>
 #include <limits.h>
+#include "xlenint.h"
+#include "sys/asm.h"
 #include "../../string/local.h"
+
+
+#if !defined(__riscv_misaligned_avoid)
+#define UNALIGNED_XLEN(X,Y) (0)
+#else
+/* Nonzero if either X or Y is not aligned to XLEN. (analogous to macro in ../../string/local.h)  */
+#define UNALIGNED_XLEN(X,Y) (((uintxlen_t)X & (SZREG-1)) | ((uintxlen_t)Y & (SZREG-1)))
+#endif
+
 
 /*SUPPRESS 20*/
 void *
 __inhibit_loop_to_libcall memmove (void *dst_void, const void *src_void, size_t length) {
 
-	char *dst = dst_void;
-	const char *src = src_void;
-	long *aligned_dst;
-	const long *aligned_src;
+	unsigned char *dst = dst_void;
+	const unsigned char *src = src_void;
+	uintxlen_t *aligned_dst;
+	const uintxlen_t *aligned_src;
 
 	if (src < dst && dst < src + length) {
 		/* Destructive overlap...have to copy backwards */
 		src += length;
 		dst += length;
 
-		if (!TOO_SMALL_LITTLE_BLOCK(length) && !UNALIGNED_X_Y(src, dst)) {
+		if (length >= SZREG && !UNALIGNED_XLEN(src, dst)) {
 
-			aligned_dst = (long*)dst;
-			aligned_src = (long*)src;
+			aligned_dst = (uintxlen_t*)dst;
+			aligned_src = (uintxlen_t*)src;
 
-			/* Copy one long word at a time if possible.  */
-			while (!TOO_SMALL_LITTLE_BLOCK(length)) {
+			/* Copy SZREG bytes at a time if possible.  */
+			while (length >= SZREG) {
 				*--aligned_dst = *--aligned_src;
-				length -= LITTLE_BLOCK_SIZE;
+				length -= SZREG;
 			}
 
 			/* Pick up any residual with a byte copier.  */
-			dst = (char*)aligned_dst;
-			src = (char*)aligned_src;
+			dst = (unsigned char*)aligned_dst;
+			src = (unsigned char*)aligned_src;
 		}
 
 		while (length--) { *--dst = *--src; }
@@ -41,29 +52,30 @@ __inhibit_loop_to_libcall memmove (void *dst_void, const void *src_void, size_t 
 		/* Use optimizing algorithm for a non-destructive copy to closely
 		   match memcpy. If the size is small or either SRC or DST is unaligned,
 		   then punt into the byte copy loop.  This should be rare.  */
-		if (!TOO_SMALL_LITTLE_BLOCK(length) && !UNALIGNED_X_Y(src, dst)) {
+		if (length >= SZREG && !UNALIGNED_XLEN(src, dst)) {
 
-			aligned_dst = (long*)dst;
-			aligned_src = (long*)src;
+			aligned_dst = (uintxlen_t*)dst;
+			aligned_src = (uintxlen_t*)src;
 
-			/* Copy 4X long words at a time if possible.  */
-			while (!TOO_SMALL_BIG_BLOCK(length)) {
+			/* Copy 4X SZREG bytes at a time if possible.  */
+			while (length >= (SZREG<<2)) {
 				*aligned_dst++ = *aligned_src++;
 				*aligned_dst++ = *aligned_src++;
 				*aligned_dst++ = *aligned_src++;
 				*aligned_dst++ = *aligned_src++;
-				length -= BIG_BLOCK_SIZE;
+				length -= (SZREG<<2);
 			}
 
-			/* Copy one long word at a time if possible.  */
-			while (!TOO_SMALL_LITTLE_BLOCK(length)) {
+
+			/* Copy SZREG bytes at a time if possible.  */
+			while (length >= SZREG) {
 				*aligned_dst++ = *aligned_src++;
-				length -= LITTLE_BLOCK_SIZE;
+				length -= SZREG;
 			}
 
 			/* Pick up any residual with a byte copier.  */
-			dst = (char*)aligned_dst;
-			src = (char*)aligned_src;
+			dst = (unsigned char*)aligned_dst;
+			src = (unsigned char*)aligned_src;
 		}
 
 		while (length--) { *dst++ = *src++; }
