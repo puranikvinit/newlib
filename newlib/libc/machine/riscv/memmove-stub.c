e775909ba4820baef4040e84e3a5af14b8612bbd
m fally <marlene.fally@gmail.com>
2025-05-04 17:57:19 +0200

    RISC-V: memmove() speed optimized: Align src if at least one address is unaligned
    Always align the source address if at least one address is unaligned.
    Load one word (or one doubleword for rv64) at a time and store back
    individual bytes.
    This change decreases the number of retired instructions if at least
    one address is unaligned. It also affects the case where both addresses
    are aligned, a destructive overlap of the memory areas is present, and
    length is not a multiple of SZREG. This is because in the case of a
    destructive overlap, the length is added to both addresses before starting
    the copying process.

--- newlib/libc/machine/riscv/memmove-stub.c
+++ newlib/libc/machine/riscv/memmove-stub.c
@@ -7,6 +7,7 @@
 #include "sys/asm.h"
 #include "../../string/local.h"
 
+
 static inline uint8_t __libc_aligned_xlen(void *dst, const void *src) {
 #if defined(__riscv_misaligned_fast)
 	return 1;
@@ -14,6 +15,7 @@ static inline uint8_t __libc_aligned_xlen(void *dst, const void *src) {
 	return !(((uintxlen_t)src & (SZREG-1)) | ((uintxlen_t)dst & (SZREG-1)));
 #endif
 }
+
 
 /*SUPPRESS 20*/
 void *
@@ -25,97 +27,166 @@ __inhibit_loop_to_libcall memmove (void *dst_void, const void *src_void, size_t 
 	const uintxlen_t *aligned_src;
 
 	if (src < dst && dst < src + length) {
-		/* Destructive overlap...have to copy backwards */
+
+		/* Case: Destructive overlap, have to copy backwards. */
 		src += length;
 		dst += length;
 
-		if (length >= SZREG && __libc_aligned_xlen(dst, src)) {
+		if (length >= SZREG) {
 
-			aligned_dst = (uintxlen_t*)dst;
-			aligned_src = (uintxlen_t*)src;
+			if (__libc_aligned_xlen(dst, src)) {
+				aligned_dst = (uintxlen_t*)dst;
+				aligned_src = (uintxlen_t*)src;
 
-			/* Copy 9X SZREG bytes at a time if possible.  */
-			while (length >= (SZREG*9)) {
-				uintxlen_t dst0 = *--aligned_src;
-				uintxlen_t dst1 = *--aligned_src;
-				uintxlen_t dst2 = *--aligned_src;
-				uintxlen_t dst3 = *--aligned_src;
-				uintxlen_t dst4 = *--aligned_src;
-				uintxlen_t dst5 = *--aligned_src;
-				uintxlen_t dst6 = *--aligned_src;
-				uintxlen_t dst7 = *--aligned_src;
-				uintxlen_t dst8 = *--aligned_src;
+				/* Copy 9X SZREG bytes at a time if possible.  */
+				while (length >= (SZREG*9)) {
+					uintxlen_t dst0 = *--aligned_src;
+					uintxlen_t dst1 = *--aligned_src;
+					uintxlen_t dst2 = *--aligned_src;
+					uintxlen_t dst3 = *--aligned_src;
+					uintxlen_t dst4 = *--aligned_src;
+					uintxlen_t dst5 = *--aligned_src;
+					uintxlen_t dst6 = *--aligned_src;
+					uintxlen_t dst7 = *--aligned_src;
+					uintxlen_t dst8 = *--aligned_src;
 
-				*--aligned_dst = dst0;
-				*--aligned_dst = dst1;
-				*--aligned_dst = dst2;
-				*--aligned_dst = dst3;
-				*--aligned_dst = dst4;
-				*--aligned_dst = dst5;
-				*--aligned_dst = dst6;
-				*--aligned_dst = dst7;
-				*--aligned_dst = dst8;
+					*--aligned_dst = dst0;
+					*--aligned_dst = dst1;
+					*--aligned_dst = dst2;
+					*--aligned_dst = dst3;
+					*--aligned_dst = dst4;
+					*--aligned_dst = dst5;
+					*--aligned_dst = dst6;
+					*--aligned_dst = dst7;
+					*--aligned_dst = dst8;
 
-				length -= SZREG*9;
+					length -= SZREG*9;
+				}
+
+				/* Copy SZREG bytes at a time if possible.  */
+				while (length >= SZREG) {
+					*--aligned_dst = *--aligned_src;
+					length -= SZREG;
+				}
+
+				/* Pick up any residual with a byte copier.  */
+				dst = (unsigned char*)aligned_dst;
+				src = (unsigned char*)aligned_src;
+
 			}
+			else if (length > (SZREG*2)) { /* Case: At least one of the addresses is unaligned. */
 
-			/* Copy SZREG bytes at a time if possible.  */
-			while (length >= SZREG) {
-				*--aligned_dst = *--aligned_src;
-				length -= SZREG;
+				/* Align src so we can load one (double)word at a time. */
+				while ((uintxlen_t)src & (SZREG-1)) {
+					*--dst = *--src;
+					length--;
+				}
+
+				aligned_src = (uintxlen_t*)src;
+
+				/* Copy SZREG bytes at a time if possible.
+				   Load one (double)word at a time and store back individual bytes.
+				 */
+				do {	uintxlen_t src_xlen = *--aligned_src;
+#if __riscv_xlen == 64
+					*--dst = (unsigned char)(src_xlen >> 56);
+					*--dst = (unsigned char)(src_xlen >> 48);
+					*--dst = (unsigned char)(src_xlen >> 40);
+					*--dst = (unsigned char)(src_xlen >> 32);
+#endif
+					*--dst = (unsigned char)(src_xlen >> 24);
+					*--dst = (unsigned char)(src_xlen >> 16);
+					*--dst = (unsigned char)(src_xlen >> 8);
+					*--dst = (unsigned char)(src_xlen);
+
+					length -= SZREG;
+
+				} while (length >= SZREG);
+
+				/* Pick up any residual with a byte copier.  */
+				src = (unsigned char*)aligned_src;
 			}
-
-			/* Pick up any residual with a byte copier.  */
-			dst = (unsigned char*)aligned_dst;
-			src = (unsigned char*)aligned_src;
 		}
 
 		while (length--) { *--dst = *--src; }
 	}
-	else {
-		/* Use optimizing algorithm for a non-destructive copy to closely
-		   match memcpy. If the size is small or either SRC or DST is unaligned,
-		   then punt into the byte copy loop.  This should be rare.  */
-		if (length >= SZREG && __libc_aligned_xlen(dst, src)) {
+	else { /* Case: Non-destructive overlap or no overlap at all. */
 
-			aligned_dst = (uintxlen_t*)dst;
-			aligned_src = (uintxlen_t*)src;
+		if (length >= SZREG) {
 
-			/* Copy 9X SZREG bytes at a time if possible.  */
-			while (length >= (SZREG*9)) {
-				uintxlen_t dst0 = *aligned_src++;
-				uintxlen_t dst1 = *aligned_src++;
-				uintxlen_t dst2 = *aligned_src++;
-				uintxlen_t dst3 = *aligned_src++;
-				uintxlen_t dst4 = *aligned_src++;
-				uintxlen_t dst5 = *aligned_src++;
-				uintxlen_t dst6 = *aligned_src++;
-				uintxlen_t dst7 = *aligned_src++;
-				uintxlen_t dst8 = *aligned_src++;
+			if (__libc_aligned_xlen(dst, src)) {
+				aligned_dst = (uintxlen_t*)dst;
+				aligned_src = (uintxlen_t*)src;
 
-				*aligned_dst++ = dst0;
-				*aligned_dst++ = dst1;
-				*aligned_dst++ = dst2;
-				*aligned_dst++ = dst3;
-				*aligned_dst++ = dst4;
-				*aligned_dst++ = dst5;
-				*aligned_dst++ = dst6;
-				*aligned_dst++ = dst7;
-				*aligned_dst++ = dst8;
+				/* Copy 9X SZREG bytes at a time if possible.  */
+				while (length >= (SZREG*9)) {
+					uintxlen_t dst0 = *aligned_src++;
+					uintxlen_t dst1 = *aligned_src++;
+					uintxlen_t dst2 = *aligned_src++;
+					uintxlen_t dst3 = *aligned_src++;
+					uintxlen_t dst4 = *aligned_src++;
+					uintxlen_t dst5 = *aligned_src++;
+					uintxlen_t dst6 = *aligned_src++;
+					uintxlen_t dst7 = *aligned_src++;
+					uintxlen_t dst8 = *aligned_src++;
 
-				length -= SZREG*9;
+					*aligned_dst++ = dst0;
+					*aligned_dst++ = dst1;
+					*aligned_dst++ = dst2;
+					*aligned_dst++ = dst3;
+					*aligned_dst++ = dst4;
+					*aligned_dst++ = dst5;
+					*aligned_dst++ = dst6;
+					*aligned_dst++ = dst7;
+					*aligned_dst++ = dst8;
+
+					length -= SZREG*9;
+				}
+
+
+				/* Copy SZREG bytes at a time if possible.  */
+				while (length >= SZREG) {
+					*aligned_dst++ = *aligned_src++;
+					length -= SZREG;
+				}
+
+				/* Pick up any residual with a byte copier.  */
+				dst = (unsigned char*)aligned_dst;
+				src = (unsigned char*)aligned_src;
+
 			}
+			else if (length > (SZREG*2)){ /* Case: At least one of the addresses is unaligned. */
 
+				/* Align src so we can load one (double)word at a time. */
+				while ((uintxlen_t)src & (SZREG-1)) {
+					*dst++ = *src++;
+					length--;
+				}
 
-			/* Copy SZREG bytes at a time if possible.  */
-			while (length >= SZREG) {
-				*aligned_dst++ = *aligned_src++;
-				length -= SZREG;
+				aligned_src = (uintxlen_t*)src;
+
+				/* Copy SZREG bytes at a time if possible.
+				   Load one (double)word at a time and store back individual bytes.
+				 */
+				do {	uintxlen_t src_xlen = *aligned_src++;
+
+					*dst++ = (unsigned char)(src_xlen);
+					*dst++ = (unsigned char)(src_xlen >> 8);
+					*dst++ = (unsigned char)(src_xlen >> 16);
+					*dst++ = (unsigned char)(src_xlen >> 24);
+#if __riscv_xlen == 64
+					*dst++ = (unsigned char)(src_xlen >> 32);
+					*dst++ = (unsigned char)(src_xlen >> 40);
+					*dst++ = (unsigned char)(src_xlen >> 48);
+					*dst++ = (unsigned char)(src_xlen >> 56);
+#endif
+					length -= SZREG;
+
+				} while (length >= SZREG);
+
+				/* Pick up any residual with a byte copier.  */
+				src = (unsigned char*)aligned_src;
 			}
-
-			/* Pick up any residual with a byte copier.  */
-			dst = (unsigned char*)aligned_dst;
-			src = (unsigned char*)aligned_src;
 		}
 
 		while (length--) { *dst++ = *src++; }
